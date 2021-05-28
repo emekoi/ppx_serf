3957028059e7f6055a68fa5a94f270c669222f6b
Emeka Nkurumeh <emekankurumeh@outlook.com>
2021-05-28 00:47:47 -0500

    added working example

--- example/test.ml
+++ example/test.ml
@@ -1,39 +1,28 @@
 (* open Lwt *)
+open Ppx_deriving_yojson_runtime
 
-type address = {
-  address_components : Yojson.Safe.t;
-  formatted_address : string;
-  geometry : Yojson.Safe.t;
-  place_id : Yojson.Safe.t;
-  types : Yojson.Safe.t;
-}
-[@@deriving yojson]
+type anime_quote = { anime : string; character : string; quote : string }
 
-type http_result = { results : address list; status : string }
-[@@deriving yojson]
+and anime_quotes = anime_quote list [@@deriving yojson]
 
-type address_of_coords = { latlng : float; key : string }
+type anime_spec = { title : string }
 [@@deriving
   serf
     {
-      url = "https://maps.googleapis.com/maps/api/geocode/json";
+      url = "https://animechan.vercel.app/api/quotes/anime";
       meth = `Get;
-      format = `Json http_result_of_yojson;
+      format = `Json anime_quotes_of_yojson;
     }]
 
 let main () =
-  let lat, lng = (float_of_string Sys.argv.(1), float_of_string Sys.argv.(2)) in
-  let%lwt _, res, _ =
-    serf_get_address_of_coords ~latlng:lat ~key:"0123456789abcdef" ~cookies:[]
-  in
+  let title = Sys.argv.(1) in
+  let%lwt _, res, _ = serf_get_anime_spec ~title ~cookies:[] in
   match res with
-  | Ok { results = addrs; status = "OK" } ->
+  | Ok res ->
       Lwt_list.iter_s
-        (fun addr ->
-          Lwt_io.printf "The coordinates %f,%f correspond to the address %s\n"
-            lat lng addr.formatted_address)
-        addrs
-  | Ok res -> Lwt_io.printf "HTTP fuckup: %s\n" res.status
+        (fun { quote; character; _ } ->
+          Lwt_io.printf "%s - \"%s\"\n\n" character quote)
+        res
   | Error msg -> Lwt_io.printf "Problem decoding response: %s\n" msg
 
 let () = Lwt_main.run (main ())
